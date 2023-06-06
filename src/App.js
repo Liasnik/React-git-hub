6928ee3929d4372c9094a4ef55ff230a35ecc9fb
Alexandr <9426247@gmail.com>
2023-06-06 20:42:06 +0300

    Initial project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TestGit from './components/Test';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>React - Git - GitHub</h1>
+      <h2>React - Git - GitHub</h2>
+     <TestGit/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
